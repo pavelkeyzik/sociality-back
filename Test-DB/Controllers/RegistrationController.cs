7d7084259c749c2700c748d407b25bc3937d612a
Pavel Keyzik <pavel.keyzik@gmail.com>
2017-12-09 15:14:16 +0300

    Added authorization and registration user

---
+++ Test-DB/Controllers/RegistrationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Test_DB.Models;
+
+namespace Test_DB.Controllers
+{
+    [Route("[controller]")]
+    public class RegistrationController : Controller
+    {
+        private readonly UserContext _context = null;
+        
+        public RegistrationController(IOptions<Settings> settings)
+        {
+            _context = new UserContext(settings);
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] User value)
+        {
+            var person = _context.Users.Find(_ => _.Login == value.Login).FirstOrDefault();
+            if (person != null)
+                return BadRequest();
+
+            try
+            {
+                value.Id = new ObjectId();
+                value.Role = "user";
+                if (value.Name == null)
+                    value.Name = value.Login;
+                
+                _context.Users.InsertOne(value);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
